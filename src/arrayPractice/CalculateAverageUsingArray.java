64cc7f56e9276d68e5acebbe75e9a433a2a94ad9
Pravin1B <pravinbajbalkar53@gmail.com>
2024-10-04 14:34:53 +0530

    add files

--- src/arrayPractice/CalculateAverageUsingArray.java
+++ src/arrayPractice/CalculateAverageUsingArray.java
@@ -1,5 +1,19 @@
-package symbiosis_training;
+package arrayPractice;
 
-public class test {
+public class CalculateAverageUsingArray {
 
+   void calculateAverageUsingArray() {
+		TakeInputArray inputarray = new TakeInputArray();
+		int[] array = inputarray.inputArray();
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		System.out.println("Average is " + (float) sum / array.length);
+	}
+
+	public static void main(String[] args) {
+		CalculateAverageUsingArray calavg = new CalculateAverageUsingArray();
+		calavg.calculateAverageUsingArray();
+	}
 }
