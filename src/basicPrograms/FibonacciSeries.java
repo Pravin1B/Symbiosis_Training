b05722e82135f56dab1120211f67dfd2f364236a
Pravin1B <pravinbajbalkar53@gmail.com>
2024-10-05 13:12:20 +0530

    add filse

---
+++ src/basicPrograms/FibonacciSeries.java
@@ -0,0 +1,32 @@
+package basicPrograms;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	static void fibonacciSeries() {
+		int a = 0, b = 1,c=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter end of the series: ");
+		int size=sc.nextInt();
+		System.out.print(a+" "+b);
+		for(int i=0; i<size; i++) {
+			
+				c=a+b;
+				if(c<size) {
+				System.out.print(" "+c);
+				a=b;
+				b=c;
+			}
+			else
+				break;
+			
+		}
+
+	}
+
+	public static void main(String[] args) {
+       fibonacciSeries();  
+	}
+
+}
