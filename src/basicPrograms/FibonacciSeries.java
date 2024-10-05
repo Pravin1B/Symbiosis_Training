06c40da54797d80abc2447beee1aa275b7d20fd7
Pravin1B <pravinbajbalkar53@gmail.com>
2024-10-05 17:38:57 +0530

    modified

--- src/basicPrograms/FibonacciSeries.java
+++ src/basicPrograms/FibonacciSeries.java
@@ -5,28 +5,27 @@ import java.util.Scanner;
 public class FibonacciSeries {
 
 	static void fibonacciSeries() {
-		int a = 0, b = 1,c=0;
-		Scanner sc=new Scanner(System.in);
+		int a = 0, b = 1, c = 0;
+		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter end of the series: ");
-		int size=sc.nextInt();
-		System.out.print(a+" "+b);
-		for(int i=0; i<size; i++) {
-			
-				c=a+b;
-				if(c<size) {
-				System.out.print(" "+c);
-				a=b;
-				b=c;
-			}
-			else
+		int size = sc.nextInt();
+		System.out.print(a + " " + b);
+		for (int i = 0; i < size; i++) {
+
+			c = a + b;
+			if (c < size) {
+				System.out.print(" " + c);
+				a = b;
+				b = c;
+			} else
 				break;
-			
+
 		}
 
 	}
 
 	public static void main(String[] args) {
-       fibonacciSeries();  
+		fibonacciSeries();
 	}
 
 }
